38a73356f8fecb27d89e1eda39e6e1abdfee2777
Belent Patrus <Bpatr024@uottawa.ca>
2022-11-21 18:49:06 -0500

    #29 Models

--- portfolio/index/models.py
+++ portfolio/index/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# table name
+
+
+class about(models.Model):
+    # table field names
+    title = models.CharField(max_length=50, blank=False)
+    description = models.TextField(max_length=800, blank=False)
+    image = models.ImageField((""), upload_to='about/', blank=False)
