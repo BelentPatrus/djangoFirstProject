6504b2207123322737f4781a82e38e981387beac
Belent Patrus <Bpatr024@uottawa.ca>
2022-11-21 18:25:05 -0500

    #28 Root Apps Change

--- portfolio/portfolio/views.py
+++ portfolio/portfolio/views.py
@@ -1,20 +1,2 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-def home(request):
-    text = {
-        'name': "Belent Patrus",
-        'age': 23,
-        'phone': 45454545,
-        'friend_name': ['Belent', 'Patrus', 'Udemy', 'Billy']
-    }
-    return render(request, 'index.html', text)
-
-
-def about(request):
-    return render(request, 'about.html')
-
-
-def contact(request):
-    return render(request, 'contact.html')
