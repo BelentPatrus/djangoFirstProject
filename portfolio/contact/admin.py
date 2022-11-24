77f8f8809a8dc9ece9afc0c3bd8c982f3bfb2c17
Belent Patrus <Bpatr024@uottawa.ca>
2022-11-23 21:03:00 -0500

    #39 data insert into database

--- portfolio/contact/admin.py
+++ portfolio/contact/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import contactlist
+from .models import contactlist, contactform
 
 
 # Register your models here.
 
 admin.site.register(contactlist)
+admin.site.register(contactform)
--- portfolio/contact/models.py
+++ portfolio/contact/models.py
@@ -10,3 +10,13 @@ class contactlist(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class contactform(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    email = models.CharField(max_length=100, blank=False)
+    subject = models.CharField(max_length=100, blank=False)
+    message = models.TextField(max_length=100, blank=False)
+
+    def __str__(self):
+        return self.name
