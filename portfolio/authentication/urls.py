27711943b2f701df3aa9747fad304b48a025d7a0
Belent Patrus <Bpatr024@uottawa.ca>
2022-11-25 18:18:42 -0500

    #42 Login System

--- portfolio/authentication/urls.py
+++ portfolio/authentication/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
-    path('registration/', views.registration, name='registration'),
+    path('', views.authlogin, name='login'),
+    path('registration/', views.authregistration, name='registration'),
     path('forget-password/', views.forgetpassword, name='forgetpassword')
 
 
