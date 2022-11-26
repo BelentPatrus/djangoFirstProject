e125ae801be72befc6bffdbabf91ee53e24e2eca
Belent Patrus <Bpatr024@uottawa.ca>
2022-11-25 21:07:50 -0500

    #44 Message Framework

--- portfolio/authentication/urls.py
+++ portfolio/authentication/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.authlogin, name='login'),
     path('registration/', views.authregistration, name='registration'),
-    path('forget-password/', views.forgetpassword, name='forgetpassword')
+    path('forget-password/', views.forgetpassword, name='forgetpassword'),
+    path('logout/', views.authlogout, name='logout')
 
 
 
--- portfolio/authentication/views.py
+++ portfolio/authentication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
@@ -13,7 +14,7 @@ def authlogin(request):
             login(request, user)
             return redirect('profile')
         else:
-            print("Invalid username and Password")
+            messages.error(request, "Invalid password and username")
 
     return render(request, 'authentication/login.html')
 
@@ -24,3 +25,9 @@ def authregistration(request):
 
 def forgetpassword(request):
     return render(request, 'authentication/forget.html')
+
+
+def authlogout(request):
+    logout(request)
+    messages.success(request, "Successfully logged out")
+    return redirect('login')
