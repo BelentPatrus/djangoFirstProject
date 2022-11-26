fa1e8e5ea503d9cdca73e5df9805e4d3c3430711
Belent Patrus <Bpatr024@uottawa.ca>
2022-11-25 21:54:57 -0500

    #45 Registration

--- portfolio/authentication/views.py
+++ portfolio/authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -20,6 +21,26 @@ def authlogin(request):
 
 
 def authregistration(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['Password']
+        confirm_password = request.POST['confirm_pass']
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, "Username not available")
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, "email not available")
+            else:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
+                user.save()
+                return redirect('profile')
+
+        else:
+            messages.error(request, "fail")
+
     return render(request, 'authentication/register.html')
 
 
